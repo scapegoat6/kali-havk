5782a00f28f473479b047999c696348ef503e8da
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-09-22 22:51:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#https://github.com/GH05T-HUNTER5/kali-havk
+# GH05T HUNTER5 KALI INSTALLATION
+if [ -d /data/data/com.termux/files/usr/bin ]; then
+echo ""
+else
+echo "Use Termux App"
+exit
+fi
 pkg install git -y
 git clone https://github.com/GH05T-HUNTER5/kali-havk >/dev/null 2>&1
 clear
